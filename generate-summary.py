f86914d90be6f4f4bf2f3f7aa8f51d5c046b4f9c
“DivyanHirasen” <divyanhirasen1092@gmail.com>
2025-06-28 20:42:45 +0200

    Add changes for summary script

--- generate-summary.py
+++ generate-summary.py
@@ -1,4 +1,5 @@
 import os
+import json
 import utils
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -10,8 +11,9 @@ SUMMARISED_HISTORY_FILE = "summarise-history.json"
 TRANSCRIPTION_DIR = "transcriptions"
 OPENROUTER_YOUTUBE_VIDEO_SUMMARISER = os.getenv("OPENROUTER_YOUTUBE_VIDEO_SUMMARISER")
 MODEL_NAME = "mistralai/mistral-small-3.2-24b-instruct:free"
+PROMPT_FILE_PATH = "prompts/prompt_1.txt"
 
-def create_and_submit_prompt():
+def create_and_submit_prompt(prompt):
     client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=OPENROUTER_YOUTUBE_VIDEO_SUMMARISER,
@@ -26,7 +28,7 @@ def create_and_submit_prompt():
     messages=[
         {
         "role": "user",
-        "content": "What is the meaning of life?"
+        "content": prompt
         }
     ]
     )
@@ -44,6 +46,13 @@ def create_prompt(prompt_file_path, data_file_path):
     final_prompt = f"{prompt_template.strip()}\n\n{data_text.strip()}"
     return final_prompt
 
+def update_summary_status(history_data, video_id, value=True):
+    for video in history_data:
+        if video.get("video_id") == video_id:
+            video["summarised"] = value
+            break  # assuming video IDs are unique
+
+
 def main():
     history_file_data = utils.read_history_file(SUMMARISED_HISTORY_FILE)
 
@@ -52,13 +61,23 @@ def main():
     print(f"Found {len(unsummarised_videos)} which need to be summarised and saved.")
 
     for video in unsummarised_videos:
-        raw_trancription_dir = 
+        print(f"Summarising: {video["video_id"]}")
+        data_file_path = f"{video["transcription_dir"]}/raw_{video["video_id"]}.txt"
+        full_prompt = create_prompt(PROMPT_FILE_PATH, data_file_path)
 
-    #summary = create_and_submit_prompt()
+        prompt_response = create_and_submit_prompt(full_prompt)
 
-    print(summary)
+        output_summary_file_path = f"{video["summarised_dir"]}/summary_{video["video_id"]}.txt"
 
+        with open(output_summary_file_path, "w") as f:
+            json.dump(prompt_response, f, indent=4)
 
+        update_summary_status(history_file_data, video["video_id"])
+
+    with open(SUMMARISED_HISTORY_FILE, "w") as f:
+        json.dump(history_file_data, f, indent=4)
+
+    print("Summary job complete")
 
 
 if __name__ == "__main__":
