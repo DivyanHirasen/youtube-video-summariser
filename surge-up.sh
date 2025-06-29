13ba88cb38437fb4966c3540497afa09ba80a787
“DivyanHirasen” <divyanhirasen1092@gmail.com>
2025-06-29 20:14:33 +0200

    Update html to be repeatable

--- surge-up.sh
+++ surge-up.sh
@@ -4,8 +4,11 @@
 # surge login --email you@example.com --token your-token
 
 # Set these values
-PROJECT_DIR="."  # or the path to your build folder
+PROJECT_DIR="website"  # or the path to your build folder
 DOMAIN="curly-parcel.surge.sh"  # customize this
+
+#rm -r website/transcriptions
+#cp -r transcriptions website
 
 # Deploy
 echo "🚀 Deploying to Surge..."
